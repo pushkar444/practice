12d842d2e47e828fb59ea214b19b40ba14ebc634
syntaxlord <pushkar.kumar.cs28@iilm.edu>
2026-05-29 09:35:16 +0530

    message

---
+++ q3.py
@@ -0,0 +1,19 @@
+
+print("Welcome to Python")
+
+# Variables
+name = "Pushkar"
+age = 20
+
+print("Name:", name)
+print("Age:", age)
+
+# Addition
+a = 10
+b = 5
+
+print("Sum =", a + b)
+
+# Simple loop
+for i in range(1, 6):
+    print(i)
